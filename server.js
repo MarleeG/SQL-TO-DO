4b5c3239787ba97426f7ca03bdcf4a8371315686
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-03-15 16:39:00 -0400

    using handlebars as template engine

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const log = console.log;
 const path = require("path");
+var exphbs  = require('express-handlebars');
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+const routes = require('./routes/routes');
+
+// handlebars
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
 
 // middleware
 app.use(express.static("public"));
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-app.get("/", (req, res) => {
-    res.send(path.join(__dirname, "index.html"));
-});
+
+
 
 app.listen(PORT, (err) => {
     if(err){
